8fecbbd599e74fbc16f671d330dbb116145c5d1d
shiva <shivacharanamdasari@gmail.com>
2025-03-18 04:43:33 +0530

    Initial Commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            //Connect to the Server running on Localhost, port 12345
+            Socket socket = new Socket("localhost", 12345);
+            System.out.println("Connected to server.");
+
+            //Input stream to communicate with the server
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //Output stream to communicate with the server
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            String message;
+            while (true) {
+                //Prompt for a message
+                System.out.println("Enter message as (exit) to quit: ");
+                message = input.readLine();
+
+                //Send the message to the server
+                output.println(message);
+
+                //If the message is (exit), break the loop and disconnect
+                if (message.equalsIgnoreCase("exit")) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                //Read the echoed response from the server and display it
+                String serverResponse = serverInput.readLine();
+                System.out.println(serverResponse);
+            }
+
+            //closing resources
+            input.close();
+            serverInput.close();
+            output.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        //Using try and catch block, because the ServerSocket throws IOException if the specified port number is invalid or already in use
+         try {
+             //Server socket to listen for client connections on specified port 12345
+             ServerSocket serverSocket = new ServerSocket(12345);
+             System.out.println("Server is waiting for a client connection...");
+
+             //Accept a client connection
+             Socket clientSocket = serverSocket.accept();
+             System.out.println("Client Connected.");
+
+             //To read data from socket object in raw bytes and convert into char set
+             InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+             BufferedReader input = new BufferedReader(inputStreamReader);
+
+             //To write the output to the output stream of the socket object
+             PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+             String clientMessage;
+             while (true) {
+                 //To read message from the client
+                 clientMessage = input.readLine(); //This blocks until a line is read
+                 System.out.println("Message received from client: "+clientMessage);
+
+                 //If the client sends "exit" as message, break the loop and close the connection
+                 if (clientMessage.equalsIgnoreCase("exit")) {
+                     System.out.println("Client disconnected.");
+                     break;
+                 }
+
+                 //Send an echo message back to the client
+                 output.println("Server: "+clientMessage);
+             }
+
+             //Closing resources
+             input.close();
+             output.close();
+             clientSocket.close();
+             serverSocket.close();
+
+         } catch (IOException e) {
+             System.out.println("The specified port is invalid or already in use "+e.getMessage());
+             e.printStackTrace();
+         }
+    }
+}
